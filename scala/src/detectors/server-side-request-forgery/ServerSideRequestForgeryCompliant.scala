18df404f8b509b21f445a784c91988548da4f66a
Khaleel Syed <khtsayed@amazon.com>
2025-09-26 17:11:09 +0530

    Added 5 cases for batch6

---
+++ scala/src/detectors/server-side-request-forgery/ServerSideRequestForgeryCompliant.scala
@@ -0,0 +1,16 @@
+import java.io.IOException
+import dispatch._
+import Defaults._
+import scala.io.{Codec, Source}
+import scalaj.http.{Http, Token}
+
+object ServerSideRequestForgeryCompliant {
+     // {fact rule=server-side-request-forgery@v1.0 defects=0}
+    def compliant(): Future[Unit] = {
+        // Compliant: The code sends a request to a fixed, known URL, mitigating the risk of server-side request forgery (SSRF).
+        val req = url("https://www.google.com")
+        val data = Http.default(req OK as.String)
+        data
+    }
+    // {/fact}
+}
